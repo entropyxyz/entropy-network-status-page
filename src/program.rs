ed3513bef6d7c8fbad9a9905b7646006446c092f
peg <ameba23@systemli.org>
2024-02-26 11:25:06 +0100

    Get program metadata, if available from program metadata web service

--- src/program.rs
+++ src/program.rs
@@ -3,6 +3,8 @@ use crate::chain_api::EntropyConfig;
 use crate::get_api_rpc;
 use crate::{display_bytes, DisplayValue};
 use anyhow::anyhow;
+use cargo_metadata::Package;
+use futures::stream::{self, StreamExt};
 use leptos::*;
 use parity_scale_codec::Decode;
 use serde::{Deserialize, Serialize};
@@ -12,6 +14,8 @@ use subxt::{
     Config, OnlineClient,
 };
 
+const PROGRAM_METADATA_SERVICE: &str = "http://127.0.0.1:3000";
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Program {
     pub hash: String,
@@ -19,10 +23,16 @@ pub struct Program {
     pub ref_counter: u128,
     pub size: usize,
     pub configurable: bool,
+    pub name: Option<String>,
 }
 
 impl Program {
-    fn new(hash: H256, program_info: ProgramInfo<AccountId32>) -> Program {
+    fn new(
+        hash: H256,
+        program_info: ProgramInfo<AccountId32>,
+        metadata: Option<Package>,
+    ) -> Program {
+        let name = metadata.map(|m| m.name);
         Program {
             hash: hash.to_string(),
             deployer: program_info.deployer.to_string(),
@@ -31,6 +41,7 @@ impl Program {
             // TODO: If configuration interface is json we could display it. Waiting till
             // we have an example of a program with a configuration interface
             configurable: !program_info.configuration_interface.is_empty(),
+            name,
         }
     }
 }
@@ -59,6 +70,11 @@ pub fn Program(program: Program) -> impl IntoView {
                     {program.configurable}
                 </p>
             </td>
+            <td class="p-4">
+                <p class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                    {program.name.unwrap_or_default()}
+                </p>
+            </td>
         </tr>
     }
 }
@@ -66,14 +82,21 @@ pub fn Program(program: Program) -> impl IntoView {
 pub async fn get_stored_programs() -> Result<Vec<Program>, ServerFnError> {
     let (api, rpc) = get_api_rpc().await?;
 
-    let programs = get_programs(&api, &rpc)
+    let programs_iter = get_programs(&api, &rpc)
         .await
         .map_err(|e| ServerFnError::ServerError(e.to_string()))?
         .into_iter()
-        .map(|(hash, program_info)| Program::new(hash, program_info))
-        .collect();
+        // .map(|(hash, program_info)| Program::new(hash, program_info))
+        // .collect();
+        .map(|(hash, program_info)| async move {
+            let metadata = get_program_metadata(hash).await;
+            Program::new(hash, program_info, metadata)
+        });
 
-    Ok(programs)
+    // Only allow 3 concurrent http requests at a time (TODO could speed things up by increasing
+    // this)
+    let programs_stream = stream::iter(programs_iter).buffer_unordered(3);
+    Ok(programs_stream.collect::<Vec<Program>>().await)
 }
 
 /// Get details of all stored programs
@@ -97,4 +120,22 @@ pub async fn get_programs(
         programs.push((H256(hash), program_info));
     }
     Ok(programs)
+}
+
+/// Attempt to get program's metadata from the http service. If there is any problem,
+/// return None
+async fn get_program_metadata(hash: H256) -> Option<Package> {
+    let response_string = reqwest::get(format!(
+        "{}/program/{}",
+        PROGRAM_METADATA_SERVICE,
+        hex::encode(hash)
+    ))
+    .await
+    .ok()?
+    .text()
+    .await
+    .ok()?;
+
+    let package: Package = serde_json::from_str(&response_string).ok()?;
+    Some(package)
 }
